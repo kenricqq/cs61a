da81c3adf3d0037249fbcc87e292c16f84609006
Kenric Tee <kenrictee@Kenrics-MacBook-Air.local>
2023-07-18 06:55:12 +0800

    week 2 done

---
+++ 1/lab00/lab00.py
@@ -0,0 +1,8 @@
+def twenty_twenty_two():
+    """Come up with the most creative expression that evaluates to 2022,
+    using only numbers and the +, *, and - operators.
+
+    >>> twenty_twenty_two()
+    2022
+    """
+    return 23 // 4 * 4 + 2 + 2 * 1000
